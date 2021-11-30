ed80555699a688bb4123fb5b2cd58d4a16ccc5c4
Maayane Soumagnac <maayanesoumagnac@Maayanes-MacBook-Pro.local>
2021-11-30 21:51:49 +0200

    version 33

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='catsHTM',
-    version='0.1.32',
+    version='0.1.33',
     description='fast access and cross-matching of large astronomical catalogs',
     #long_description=long_description,
     #long_description_content_type='text/markdown',
